bc49ac716479d5cb5fb884cd85ffe05c9656b146
Stefanie Contreras <stefsnow@gmail.com>
2014-06-07 19:29:11 -0600

    Added working code for filter and reject

--- src/underbar.js
+++ src/underbar.js
@@ -87,7 +87,7 @@ var _ = {};
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
 	var result = [ ];
-	_.each(collection, function(item, test) {
+	_.each(collection, function(item) {
 		if(test(item) ){
 			result.push(item);
 		}
@@ -99,6 +99,11 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+		var result = [ ];
+		result = _.filter(collection, function(item){
+				return !test(item);
+		});
+		return result;
   };
 
   // Produce a duplicate-free version of the array.
